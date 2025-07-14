5055575bb8878d72c0e3d1c90e79db6808482e65
Roman <66372309+transinffo@users.noreply.github.com>
2025-07-15 01:49:19 +0300

    Update scrape.js

--- scrape.js
+++ scrape.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 
 (async () => {
   try {
-    const placeId = 'ChIJ4zvW9oPz1EAROeO3nS1OcS8'; // ЗАМЕНИ НА СВОЙ Place ID
+    const placeId = 'ChIJf1l0juHP1EAR9vrR9S19wGk'; // ЗАМЕНИ НА СВОЙ Place ID
     const reviews = await scrapeReviews({ placeId, language: 'uk' });
 
     fs.writeFileSync('reviews.json', JSON.stringify(reviews, null, 2));
